91376465fdc6ac561eb80bf4a73542f3b20b8a39
Saba Pochkhua <spochkhua@velitech.com>
2024-04-04 20:20:58 +0400

    chore: init ecs

---
+++ infra/main.tf
@@ -0,0 +1,228 @@
+provider "aws" {
+  region  = "eu-central-1"
+  profile = "dfl"
+}
+
+# Vpc
+resource "aws_vpc" "ecs_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "ecs_vpc"
+  }
+}
+
+resource "aws_subnet" "ecs_subnet_1" {
+  vpc_id                  = aws_vpc.ecs_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "ecs_subnet_1"
+  }
+}
+
+resource "aws_subnet" "ecs_subnet_2" {
+  vpc_id                  = aws_vpc.ecs_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-central-1b"  # Adjust the AZs to your region
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "ecs_subnet_2"
+  }
+}
+
+
+resource "aws_internet_gateway" "ecs_igw" {
+  vpc_id = aws_vpc.ecs_vpc.id
+
+  tags = {
+    Name = "ecs_igw"
+  }
+}
+
+resource "aws_route_table" "ecs_route_table" {
+  vpc_id = aws_vpc.ecs_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ecs_igw.id
+  }
+
+  tags = {
+    Name = "ecs_route_table"
+  }
+}
+
+resource "aws_route_table_association" "ecs_rta_1" {
+  subnet_id      = aws_subnet.ecs_subnet_1.id
+  route_table_id = aws_route_table.ecs_route_table.id
+}
+
+resource "aws_route_table_association" "ecs_rta_2" {
+  subnet_id      = aws_subnet.ecs_subnet_2.id
+  route_table_id = aws_route_table.ecs_route_table.id
+}
+
+# Ecs Cluster
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "my-ecs-cluster"
+}
+
+
+# Ecs Task Definition
+resource "aws_ecs_task_definition" "ecs_task" {
+  family                   = "my-ecs-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "my-app"
+      image     = "nginx" # Example image
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+# Load Balancer
+resource "aws_lb" "ecs_alb" {
+  name               = "ecs-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.ecs_subnet_1.id, aws_subnet.ecs_subnet_2.id]
+  security_groups    = [aws_security_group.alb_sg.id]
+
+  tags = {
+    Name = "ecs_alb"
+  }
+}
+
+
+resource "aws_lb_target_group" "ecs_tg" {
+  name     = "ecs-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.ecs_vpc.id
+  target_type = "ip"  # Set the target type to IP for compatibility with awsvpc network mode
+
+  health_check {
+    protocol           = "HTTP"
+    path               = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "ecs_listener" {
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+  }
+}
+
+# Ecs Service
+resource "aws_ecs_service" "ecs_service" {
+  name            = "my-ecs-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.ecs_subnet_1.id, aws_subnet.ecs_subnet_2.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_tasks_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+    container_name   = "my-app"
+    container_port   = 80
+  }
+}
+
+# Security Groups
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.ecs_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_tasks_sg" {
+  vpc_id = aws_vpc.ecs_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# 
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "ecsExecutionRole"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
